6217af2d230a9f3e02618b5374037ef62918cd6e
meahow <pszona@gmail.com>
2019-04-02 08:53:45 +0200

    day 9 contd

--- 2017/09/solution1.py
+++ 2017/09/solution1.py
@@ -6,12 +6,16 @@ def solve(line):
     return parse(line, 0)
 
 def parse(s, level):
-    points = 0
+    points = level
     idx = 0
-    while s[idx] != '{':
-        idx += 1
-    parse(s[idx:], level + 1)
-    return points
+    while idx <= len(s) < 1:
+        if s[idx] == '{':
+            new_idx, new_points = parse(s[idx + 1:], level + 1)
+            points += new_points
+            idx += new_idx
+        else:
+            idx += 1
+    return idx, points
 
 if __name__ == '__main__':
     with open("input.txt") as infile:
