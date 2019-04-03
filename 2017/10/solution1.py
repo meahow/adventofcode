19c51a8e6784f1b24075a93c2d606dc3d7a6a67b
meahow <pszona@gmail.com>
2019-04-03 09:30:14 +0200

    day 10 part 1

--- 2017/10/solution1.py
+++ 2017/10/solution1.py
@@ -1,8 +1,25 @@
 # python3
 
 
-def solve(thelist, lengths):
-    assert len(thelist) > len(lengths)
+def solve(thelist, skiplist):
+    lenlist = len(thelist)
+    idx = 0
+    for skip in range(len(skiplist)):
+        length = skiplist[skip]
+        stop = idx + length
+        rest = -1
+        current_list = thelist[idx:stop]
+        if stop > lenlist:
+            rest = stop - lenlist
+            current_list.extend(thelist[:rest])
+        current_list.reverse()
+        # update all items
+        for i in range(length):
+            tmp_idx = (i + idx) % lenlist
+            thelist[tmp_idx] = current_list[i]
+        jump = length + skip
+        idx += jump
+        idx %= lenlist
     return thelist[0] * thelist[1]
 
 if __name__ == '__main__':
